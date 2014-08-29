b7500812777dc42f70a924f291d623ce6f0bcde0
root <root@STXTS00120.DMZ>
2014-08-29 11:16:36 +0200

    Added new 881-2 page-jobs

---
+++ jobs/txt881.rb
@@ -0,0 +1,12 @@
+# :first_in sets how long it takes before the job is first run. In this case, it is run immediately
+require 'net/http'
+require 'retriable'
+require 'rexml/document'
+include REXML
+
+SCHEDULER.every '10m', :first_in => '15s' do |job|
+  img = "http://api.teletext.ch/online/pics/medium/SRF1_881-1.gif"
+
+  send_event('txt881', {image: img })
+end
+
