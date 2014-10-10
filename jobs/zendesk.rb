0b4dd64cbe0ad256b8854a128f300a5626f75490
SWISS TXT <servicedesk@swisstxt.ch>
2014-10-10 12:36:12 +0200

    Background color for ticket

--- jobs/zendesk.rb
+++ jobs/zendesk.rb
@@ -3,16 +3,15 @@ require 'net/http'
 require 'net/https'
 
 
-SCHEDULER.every '10s', :first_in => '3s' do |job|
+SCHEDULER.every '60s', :first_in => '5s' do |job|
   begin
     data = Stxtdashing.fetch_http(settings.config["zendesk"]["url"],settings.config["zendesk"]["path"],settings.config["zendesk"]["ssl"],settings.config["zendesk"]["user"],settings.config["zendesk"]["pw"])
 
     data = JSON.parse(data)
     ticket_count = data['count']
-    icon = if (ticket_count<1) then "thumbs-up" else "thumbs-down" end
+    status = if (ticket_count<1) then "ok" else "danger" end
 
-    send_event("zendesk",{title: "New Tickets", icon: icon, text: ticket_count})
+    send_event("zendesk",{title: "New Tickets", status: status, text: ticket_count})
   
   end
 end
-  
