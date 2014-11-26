c951c8f3cf3f07edf2a830a7115e50851131810b
Julien-CH <Julien-CH@users.noreply.github.com>
2014-11-26 14:22:52 +0100

    Added Mediahub requests

--- jobs/zendesk.rb
+++ jobs/zendesk.rb
@@ -5,6 +5,7 @@ require 'net/https'
 
 SCHEDULER.every '60s', :first_in => '5s' do |job|
   begin
+  #Swisstxt requests
     data = Stxtdashing.fetch_http(settings.config["zendesk"]["url"],settings.config["zendesk"]["path"],settings.config["zendesk"]["ssl"],settings.config["zendesk"]["user"],settings.config["zendesk"]["pw"])
 
     data = JSON.parse(data)
@@ -12,6 +13,16 @@ SCHEDULER.every '60s', :first_in => '5s' do |job|
     status = if (ticket_count<1) then "ok" else "danger" end
 
     send_event("zendesk",{title: "New Tickets", status: status, text: ticket_count})
+    
+  #Mediahub requests  
+    data = Stxtdashing.fetch_http(settings.config["mediahub"]["url"],settings.config["mediahub"]["path"],settings.config["mediahub"]["ssl"],settings.config["mediahub"]["user"],settings.config["mediahub"]["pw"])
+
+    data = JSON.parse(data)
+    
+    ticket_count = data['count']
+    status = if (ticket_count<1) then "ok" else "danger" end
+  
+    send_event("mediahub",{title: "New Mediahub Request", status: status, text: ticket_count})
   
   end
 end
